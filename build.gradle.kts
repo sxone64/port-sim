022c96e16f7c44389767223a0e405369651e47a0
Nebojša <ynebojsatolimiry@gmail.com>
2026-08-25 13:03:01 +0200

    chore: add code analysis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.jetbrains:annotations:26.1.0")
 }
 
 tasks.test {
