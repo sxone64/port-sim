e83a9e1ce1850dad325277a4d83d3547d2d1542b
Nebojša <ynebojsatolimiry@gmail.com>
2026-08-26 16:12:47 +0200

    feat: add exceptions logger

--- src/main/java/portsim/Main.java
+++ src/main/java/portsim/Main.java
@@ -1,7 +1,14 @@
 package portsim;
 
+import portsim.io.AppLogger;
+
 public class Main {
     static void main() {
-        System.out.println("Hello World!");
+        // All exception that aren't caught are logged as severe
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) ->
+                AppLogger.getInstance().severe(
+                        "Uncaught exception in %s".formatted(thread.getName()),
+                        throwable)
+        );
     }
 }
---
+++ src/main/java/portsim/io/AppLogger.java
@@ -0,0 +1,53 @@
+package portsim.io;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import static java.util.logging.Level.*;
+
+public final class AppLogger {
+    private static final Logger LOGGER = Logger.getLogger(AppLogger.class.getName());
+    private static final AppLogger INSTANCE = new AppLogger();
+
+    public static AppLogger getInstance() {
+        return INSTANCE;
+    }
+
+    private AppLogger() {
+        try {
+            var errorHandler = new FileHandler("error.log", true);
+            errorHandler.setFormatter(new SimpleFormatter());
+            errorHandler.setLevel(ALL);
+            LOGGER.addHandler(errorHandler);
+
+            var warningHandler = new FileHandler("warning.log", true);
+            warningHandler.setFormatter(new SimpleFormatter());
+            warningHandler.setLevel(WARNING);
+            warningHandler.setFilter(record -> record.getLevel().equals(WARNING));
+            LOGGER.addHandler(warningHandler);
+
+            // Warnings are also logged on console
+            var consoleHandler = new ConsoleHandler();
+            consoleHandler.setFormatter(new SimpleFormatter());
+            consoleHandler.setLevel(WARNING);
+            consoleHandler.setFilter(record -> record.getLevel().equals(WARNING));
+            LOGGER.addHandler(consoleHandler);
+
+            LOGGER.setUseParentHandlers(false);
+            LOGGER.setLevel(ALL);
+        } catch (IOException e) {
+            System.err.println("Failed to initialize logger: " + e.getMessage());
+        }
+    }
+
+    public void severe(String message, Throwable t) {
+        LOGGER.log(SEVERE, message, t);
+    }
+
+    public void warning(String message, Throwable t) {
+        LOGGER.log(WARNING, message, t);
+    }
+}
