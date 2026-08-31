86f4fe9dff800fe97326884554cd36066843ce4b
Nebojša <ynebojsatolimiry@gmail.com>
2026-08-31 17:27:38 +0200

    feat: add port service

---
+++ src/main/java/portsim/service/PortService.java
@@ -0,0 +1,50 @@
+package portsim.service;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+import portsim.io.PortPersistence;
+import portsim.model.Port;
+import portsim.model.Terminal;
+
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+public final class PortService {
+    private static final PortService INSTANCE = new PortService();
+
+    public static PortService getInstance() {
+        return INSTANCE;
+    }
+
+    private final Port port;
+
+    private PortService() {
+        port = PortPersistence.getInstance().loadOrCreate();
+    }
+
+    public int getTotalShips() {
+        return getTotalCount(terminal -> terminal.getShips().size());
+    }
+
+    public int getTotalStateShips() {
+        return getTotalCount(Terminal::getStateShipCount);
+    }
+
+    public int getTotalDocks() {
+        return getTotalCount(Terminal::getNumDocks);
+    }
+
+    public int getTotalFreeDocks() {
+        return getTotalCount(Terminal::getFreeDocks);
+    }
+
+    public @NotNull @Unmodifiable List<Terminal> getTerminals() {
+        return List.copyOf(port.terminals());
+    }
+
+    private int getTotalCount(ToIntFunction<Terminal> mapper) {
+        return port.terminals().stream()
+                .mapToInt(mapper)
+                .sum();
+    }
+}
