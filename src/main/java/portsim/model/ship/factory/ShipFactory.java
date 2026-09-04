bae96753d48310034bb96dd66fed0acaa0efb1bf
Nebojša <ynebojsatolimiry@gmail.com>
2026-09-04 12:58:49 +0200

    fix: make TypeNotSupportedException publicly accessible

--- src/main/java/portsim/model/ship/factory/ShipFactory.java
+++ src/main/java/portsim/model/ship/factory/ShipFactory.java
@@ -9,7 +9,7 @@ import portsim.model.ship.state.impl.*;
 import java.util.Map;
 import java.util.function.Function;
 
-class ShipFactory {
+public class ShipFactory {
     private static final ShipFactory INSTANCE = new ShipFactory();
 
     private static final Map<Class<? extends Ship>, Function<ShipBuilder, Ship>> FACTORIES = Map.of(
