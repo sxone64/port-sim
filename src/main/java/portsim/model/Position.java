cbd49eaae23d95e04a3b7b4006d0d159b7178ced
Nebojša <ynebojsatolimiry@gmail.com>
2026-09-23 12:41:02 +0200

    feat: add toString to position

--- src/main/java/portsim/model/Position.java
+++ src/main/java/portsim/model/Position.java
@@ -1,9 +1,16 @@
 package portsim.model;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serial;
 import java.io.Serializable;
 
 public record Position(int row, int column) implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Override
+    public @NotNull String toString() {
+        return "(%d, %d)".formatted(row, column);
+    }
 }
